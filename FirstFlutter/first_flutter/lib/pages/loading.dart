a3deab38d71eee78215ce61dfedab3bfe1c9de46
SwordleihsUcll <43514236+SwordleihsUcll@users.noreply.github.com>
2019-11-15 10:52:22 +0100

    lodiang

--- FirstFlutter/first_flutter/lib/pages/loading.dart
+++ FirstFlutter/first_flutter/lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import '../widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../classes/http_service.dart';
 
 import 'home.dart';
 
