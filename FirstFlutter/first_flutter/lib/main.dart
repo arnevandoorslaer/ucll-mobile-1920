6efce4852af1c4556efcfc32d13d03f4f9f0508b
BelgianNoise <king.arthur360@gmail.com>
2019-12-30 03:35:29 +0100

    zwarte balk

--- FirstFlutter/first_flutter/lib/main.dart
+++ FirstFlutter/first_flutter/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:Cleverdivide/pages/loadingExpenses.dart';
 import 'package:Cleverdivide/pages/expensesList.dart';
 import 'package:Cleverdivide/pages/addExpense.dart';
 import 'package:Cleverdivide/pages/profileScreen.dart';
+import 'package:flutter/services.dart';
 import 'pages/addEvent.dart';
 import 'pages/loading.dart';
 import 'pages/loginScreen.dart';
@@ -16,31 +17,36 @@ import 'package:flutter/material.dart';
 import 'pages/eventScreen.dart';
 import 'pages/home.dart';
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  initialRoute: "/",
-  routes: {
-    "/" : (context) => Loading(),
-    "/home" : (context) => Home(),
-    "/add" : (context) => AddEvent(),
-    "/login" : (context) => LoginScreen(),
-    "/event" : (context) => EventScreen(),
-    "/register" : (context) => RegisterScreen(),
-    "/participants" : (context) => ParticipantList(),
-    "/loadingparticipants" : (context) => LoadingParticipants(),
-    "/loadingevent" : (context) => LoadingEvent(),
-    "/loadingaddparticipants" : (context) => LoadingAddParticipants(),
-    "/addparticipants" : (context) => AddParticipants(),
-    "/loadingexpenses" : (context) => LoadingExpenses(),
-    "/expenses" : (context) => ExpensesList(),
-    "/addexpense" : (context) => AddExpense(),
-    "/loadingaddexpense" : (context) => LoadingAddExpense(),
-    "/profile" : (context) => ProfileScreen(),
-    "/loadingProfile" : (context) => ProfileScreen(),
-  },
-  theme: ThemeData(
-    brightness: Brightness.dark,
-    accentColor: Colors.amber,
-    toggleableActiveColor: Color(0xff00285A),
-  ),
-));
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: "/",
+    routes: {
+      "/": (context) => Loading(),
+      "/home": (context) => Home(),
+      "/add": (context) => AddEvent(),
+      "/login": (context) => LoginScreen(),
+      "/event": (context) => EventScreen(),
+      "/register": (context) => RegisterScreen(),
+      "/participants": (context) => ParticipantList(),
+      "/loadingparticipants": (context) => LoadingParticipants(),
+      "/loadingevent": (context) => LoadingEvent(),
+      "/loadingaddparticipants": (context) => LoadingAddParticipants(),
+      "/addparticipants": (context) => AddParticipants(),
+      "/loadingexpenses": (context) => LoadingExpenses(),
+      "/expenses": (context) => ExpensesList(),
+      "/addexpense": (context) => AddExpense(),
+      "/loadingaddexpense": (context) => LoadingAddExpense(),
+      "/profile": (context) => ProfileScreen(),
+      "/loadingProfile": (context) => ProfileScreen(),
+    },
+    theme: ThemeData(
+      brightness: Brightness.dark,
+      accentColor: Colors.amber,
+      toggleableActiveColor: Color(0xff00285A),
+    ),
+  )
+  );
+}
+
