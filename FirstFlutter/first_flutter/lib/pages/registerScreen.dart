cb304e1dd4e4997d4043218676a35ea7365099b1
SwordleihsUcll <43514236+SwordleihsUcll@users.noreply.github.com>
2020-01-17 13:17:06 +0100

    Profile button removed from register screen

--- FirstFlutter/first_flutter/lib/pages/registerScreen.dart
+++ FirstFlutter/first_flutter/lib/pages/registerScreen.dart
@@ -1,4 +1,3 @@
-import 'package:Cleverdivide/widgets/appbar.dart';
 import 'package:Cleverdivide/widgets/registerForm.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +10,20 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBarWidget(text: "Registreren",),
+      appBar: AppBar(
+        title: Text("Registreren"),
+        backgroundColor: Color(0xff00285A),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {
+              Navigator.of(context).popUntil((route) => route.isFirst);
+              Navigator.pushReplacementNamed(context, "/");
+            },
+          ),
+        ],
+      ),
+
       body: RegisterForm(),
       backgroundColor: Colors.grey[900],
     );
